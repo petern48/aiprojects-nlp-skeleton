9e498bd9bb188e5ce878c33d1a6a6b893319ceeb
alicia yu <yu.alicia@gmail.com>
2023-10-23 18:29:29 -0700

    Update dimensions of starting network

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -9,17 +9,17 @@ class StartingNetwork(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(12122002, 50) # What could that number mean!?!?!? Ask an officer to find out :)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(289, 50) # What could that number mean!?!?!? Ask an officer to find out :)
+        self.fc2 = nn.Linear(50, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         '''
         x (tensor): the input to the model
         '''
-        x = self.fc1(x)
+        x = self.fc1(x.to(torch.float32))
         x = self.fc2(x)
         x = self.sigmoid(x)
-        return x
+        return x.reshape(-1, 1)
 
 
